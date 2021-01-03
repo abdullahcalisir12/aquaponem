c8859c4846266629e418700f4ed181d1a6bceb8b
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-04 01:28:39 +0300

    spacing variables added

---
+++ src/app-shared/spacing.ts
@@ -0,0 +1,31 @@
+import { css } from 'lit-element';
+
+export const spacing = css`
+  :host {
+    --spc-0: 0;
+    --spc-1: .25rem;
+    --spc-2: .5rem;
+    --spc-3: .75rem;
+    --spc-4: 1rem;
+    --spc-5: 1.25rem;
+    --spc-6: 1.5rem;
+    --spc-7: 1.75rem;
+    --spc-8: 2rem;
+    --spc-10: 2.5rem;
+    --spc-12: 3rem;
+    --spc-14: 3.5rem;
+    --spc-16: 4rem;
+    --spc-20: 5rem;
+    --spc-24: 6rem;
+    --spc-28: 7rem;
+    --spc-32: 8rem;
+    --spc-36: 9rem;
+    --spc-40: 10rem;
+    --spc-48: 12rem;
+    --spc-56: 14rem;
+    --spc-64: 16rem;
+    --spc-72: 18rem;
+    --spc-80: 20rem;
+    --spc-96: 24rem;
+  }
+`;
