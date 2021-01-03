0b539e7fe25d3fd9f467d30b1fabfd0611ede8f7
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-03 14:30:39 +0300

    link component created

---
+++ src/link/index.ts
@@ -0,0 +1,34 @@
+import { LitElement, html, customElement, property } from 'lit-element';
+import { classMap } from 'lit-html/directives/class-map';
+import { ifDefined } from 'lit-html/directives/if-defined';
+
+import { buttonStyles } from '../theme/button';
+import { prefix } from '../constant';
+
+@customElement(`${prefix}-link`)
+export class Link extends LitElement {
+
+  @property() href: string = '#';
+  @property() variant: string = 'primary';
+  @property() size: string = 'base';
+  @property() round: string = 'base';
+  @property() label: string = 'anchor';
+
+  static get styles() {
+    return [
+      buttonStyles
+    ];
+  }
+
+  render() {
+    return html`
+      <a
+        aria-label=${ifDefined(this.label)}
+        class="${classMap({ [`btn-s-${this.size}`]: true, [`btn-r-${this.round}`]: true })}"
+        href="${this.href}"
+        style="--color: var(--c-${this.variant});">
+        <slot></slot>
+      </a>
+    `;
+  }
+}
