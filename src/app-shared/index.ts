8c35cf5c067bd492e5fe79c3f0f2a70886b9dbec
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-04 01:28:26 +0300

    new variables exported

--- src/app-shared/index.ts
+++ src/app-shared/index.ts
@@ -1,4 +1,6 @@
 export { themeColors, colors } from './colors';
-export { textStyles } from './typography';
+export { textStyles, fontFamily, fontWeight } from './typography';
 export { buttonStyles } from './button';
 export { appStyles } from './app-styles';
+export { spacing } from './spacing';
+export { borderWidth, borderRadius } from './border';
