278c063ae86cb9c5d017d4229f05d858f44192d1
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-06 01:39:35 +0300

    container exported and usage added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { App } from '@/app';
 export { Theme } from '@/theme';
+export { Container } from '@/container';
 export { Card } from '@/card';
 export { Text } from '@/text';
 export { Button, Link } from '@/button';
