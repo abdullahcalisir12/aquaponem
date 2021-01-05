1d66d82ebffc1ad63be9072519f6127e67f4e665
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-06 01:39:03 +0300

    container component created

---
+++ src/container/index.ts
@@ -0,0 +1,27 @@
+import { LitElement, html, customElement, property, css } from 'lit-element';
+import { containerStyle } from './style';
+import { prefix } from '@constants';
+import { classMap } from 'lit-html/directives/class-map';
+
+@customElement(`${prefix}-container`)
+export class Container extends LitElement {
+  @property() size: string = 'fluid';
+  @property() center: boolean = true;
+
+  static get styles() {
+    return [
+      containerStyle,
+    ];
+  }
+
+  render() {
+    return html`
+      <div class="${classMap({
+          container: true,
+          [`container-${this.size}`]: true,
+        })}">
+        <slot></slot>
+      </div>
+    `;
+  }
+}
