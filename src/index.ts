60aba57052d228d66912a3988037be67838f6039
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-06 03:21:35 +0300

    grid components exported

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export { App } from '@/app';
 export { Theme } from '@/theme';
 export { Container } from '@/container';
+export { Row } from '@/row';
+export { Column } from '@/column';
 export { Card } from '@/card';
 export { Text } from '@/text';
 export { Button, Link } from '@/button';
