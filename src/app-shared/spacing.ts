51b8ddcf44f40cd6d9d2754d3001003eea7ed141
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-07 05:15:45 +0300

    default spapcing variable added

--- src/app-shared/spacing.ts
+++ src/app-shared/spacing.ts
@@ -1,5 +1,33 @@
 import { css } from 'lit-element';
 
+const defaultSpacings = {
+  0: '0',
+  1: '.25rem',
+  2: '.5rem',
+  3: '.75rem',
+  4: '1rem',
+  5: '1.25rem',
+  6: '1.5rem',
+  7: '1.75rem',
+  8: '2rem',
+  10: '2.5rem',
+  12: '3rem',
+  14: '3.5rem',
+  16: '4rem',
+  20: '5rem',
+  24: '6rem',
+  28: '7rem',
+  32: '8rem',
+  36: '9rem',
+  40: '10rem',
+  48: '12rem',
+  56: '14rem',
+  64: '16rem',
+  72: '18rem',
+  80: '20rem',
+  96: '24rem',
+}
+
 export const spacing = css`
   :host {
     --spc-0: 0;
