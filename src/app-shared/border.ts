6466f134d4083129a5ff05c222af09b2f12ac672
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-07 05:12:27 +0300

    shared variables starting create

--- src/app-shared/border.ts
+++ src/app-shared/border.ts
@@ -1,5 +1,22 @@
 import { css } from 'lit-element';
 
+const defaultBorderRadius = {
+  'none': '0',
+  'sm': '.25rem',
+  'md': '.5rem',
+  'lg': '.75rem',
+  'xl': '1rem',
+  'full': '9999px',
+}
+
+const defaultBorderWidth = {
+  none: '0',
+  1: '1px',
+  2: '2px',
+  3: '3px',
+  4: '4px',
+}
+
 export const borderRadius = css`
   :host {
     --br-none: 0;
