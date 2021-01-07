055974be03fba25743cfafdee3c5752f295e4ef8
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-07 05:13:02 +0300

    breakpoints variables created

--- src/app-shared/breakpoints.ts
+++ src/app-shared/breakpoints.ts
@@ -1,11 +1,19 @@
 import { css } from 'lit-element';
 
+export const defaultBreakpoints = {
+  sm: '768px',
+  md: '1024px',
+  lg: '1280px',
+  xl: '1440px',
+  '2xl': '1920px',
+};
+
 export const breakpoints = css`
   :host {
-    --bp-sm: 768px,
-    --bp-md: 1024px,
-    --bp-lg: 1280px,
-    --bp-xl: 1440px,
-    --bp-2xl: 1920px,
+    --bp-sm: 768px;
+    --bp-md: 1024px;
+    --bp-lg: 1280px;
+    --bp-xl: 1440px;
+    --bp-2xl: 1920px;
   }
 `;
