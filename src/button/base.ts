96b5614c0e74fc0293cb29009597a297cae38318
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-07 05:14:13 +0300

    variant prop re moved color used instead

--- src/button/base.ts
+++ src/button/base.ts
@@ -1,12 +1,11 @@
 import { LitElement, property } from 'lit-element';
 import { classMap } from 'lit-html/directives/class-map';
-
+import { sharedColors } from '@shared';
 import { buttonStyles } from './style';
 
 export class Base extends LitElement {
   @property() round: string = 'md';
   @property() size: string = 'base';
-  @property() variant: string = 'primary';
   @property() color: string|null = null;
 
   static get styles() {
@@ -20,7 +19,7 @@ export class Base extends LitElement {
   }
 
   public get styles() {
-    const color = this.color ? this.color : `var(--c-${this.variant})`;
+    const color = this.color && Object.keys(sharedColors).includes(this.color) ? `var(--c-${this.color})` : this.color;
     return `
       --color: ${color};
       --btn-br: var(--br-${this.round})
