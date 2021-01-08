f6a529867ad32b983db00fd855517f01519c56e7
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-08 21:01:31 +0300

    base btn type safety

--- src/button/base.ts
+++ src/button/base.ts
@@ -1,4 +1,4 @@
-import { html, property } from 'lit-element';
+import { CSSResultArray, html, property, TemplateResult } from 'lit-element';
 import { classMap } from 'lit-html/directives/class-map';
 import { VariantManager } from '@shared';
 import { buttonStyles } from './style';
@@ -7,17 +7,17 @@ export class Base extends VariantManager {
   @property() round: string = 'md';
   @property() size: string = 'base';
 
-  static get styles() {
+  static get styles(): CSSResultArray {
     return [
       buttonStyles
     ];
   }
 
-  public get classes() {
+  public get classes(): Object {
     return classMap({ [`btn-s-${this.size}`]: true });
   }
 
-  public get styles() {
+  public get styles(): TemplateResult {
     return this.withVariant(html`
       <style>
         :host {
