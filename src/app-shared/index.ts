8c76a96bf20536aa98f22db16230afe09b0fce19
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-08 21:03:04 +0300

    field wrapper exported

--- src/app-shared/index.ts
+++ src/app-shared/index.ts
@@ -3,3 +3,4 @@ export { breakpoints } from './breakpoints';
 export { themeColors, colors, sharedColors, VariantManager } from './colors';
 export { borderWidth, borderRadius } from './border';
 export { textStyles, fontFamily, fontWeight } from './typography';
+export { FieldWrapper } from './FieldWrapper';
