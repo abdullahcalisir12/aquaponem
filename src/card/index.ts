9776a5e6dd984ed34456697f5759d3fa31ee9d1e
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-08 03:59:01 +0300

    card component updated

--- src/card/index.ts
+++ src/card/index.ts
@@ -1,30 +1,42 @@
-import { LitElement, html, css, customElement } from 'lit-element';
+import { html, css, customElement } from 'lit-element';
 import { prefix } from '@constants';
+import { VariantManager } from '@/app-shared';
 
 @customElement(`${prefix}-card`)
-export class Card extends LitElement {
+export class Card extends VariantManager {
 
   static get styles() {
     return [
       css`
         :host {
           display: block;
-          border-radius: var(--br-base);
         }
 
         .card {
           display: flex;
+          flex-direction: column;
+          background-color: var(--color);
+          border-radius: var(--br-lg);
+          padding: var(--spc-5) var(--spc-6);
+        }
+
+        ::slotted([slot="header"]) {
+          margin-bottom: var(--spc-3);
         }
 
         .body {
           flex: 1;
+        }
+
+        ::slotted([slot="footer"]) {
+          margin-top: var(--spc-3);
         }
       `,
     ];
   }
 
   render() {
-    return html`
+    return this.withVariant(html`
       <div class="card">
         <slot name="header"></slot>
         <div class="body">
@@ -32,6 +44,6 @@ export class Card extends LitElement {
         </div>
         <slot name="footer"></slot>
       </div>
-    `;
+    `);
   }
 }
