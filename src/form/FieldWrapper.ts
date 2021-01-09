52cbd50ed58f12a9edfa0c0a222e2dbcea716a4e
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-09 19:03:57 +0300

    field wrapper moved to form folder

---
+++ src/form/FieldWrapper.ts
@@ -0,0 +1,48 @@
+import { VariantManager } from "@shared";
+import { html, css, property, CSSResult, customElement } from "lit-element";
+
+@customElement('field-wrapper')
+export class FieldWrapper extends VariantManager {
+  @property() label: string = '';
+
+  static get styles(): CSSResult {
+    return css`
+      :host {
+        display: block;
+      }
+
+      fieldset {
+        all: unset;
+        display: flex;
+        position: relative;
+        border: 2px solid var(--color);
+        border-radius: var(--br-md);
+        background-color: var(--c-dark) !important;
+      }
+
+      legend {
+        padding: 0 .5rem;
+        margin-left: 1rem;
+        color: var(--color);
+      }
+
+      ::slotted(input), ::slotted(textarea) {
+        all: unset;
+        display: block;
+        height: 100%;
+        width: 100%;
+        padding: .75rem 1.5rem 1rem;
+        font-size: 1.25rem;
+      }
+    `;
+  }
+
+  render() {
+    return this.withVariant(html`
+      <fieldset part="fieldset">
+        <legend part="legend">${this.label}</legend>
+        <slot></slot>
+      </fieldset>
+    `);
+  }
+}
