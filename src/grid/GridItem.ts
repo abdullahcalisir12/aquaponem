296931eb3fd8ce62a56a6bff98068b1932816b8d
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-09 22:15:29 +0300

    grid item component is created

---
+++ src/grid/GridItem.ts
@@ -0,0 +1,23 @@
+import { prefix } from "@/app-constants";
+import { customElement, html, LitElement, property } from "lit-element";
+
+@customElement(`${prefix}-grid-item`)
+export class GridItem extends LitElement {
+  @property() col: string|undefined;
+  @property() row: string|undefined;
+  @property() area: string|undefined;
+  @property() place: string|undefined = 'center';
+
+  updated(changedProperties: Map<string, string>) {
+    if (this.col && changedProperties.has('col')) this.style.gridColumn = this.col;
+    if (this.row && changedProperties.has('row')) this.style.gridRow = this.row;
+    if (this.area && changedProperties.has('area')) this.style.gridArea = this.area;
+    if (this.place && changedProperties.has('place')) this.style.placeSelf = this.place;
+  }
+
+  render() {
+    return html`
+      <slot></slot>
+    `;
+  }
+}
