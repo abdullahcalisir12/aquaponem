983c3c9f64e7c38576cc67c0419fa293f6c92910
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-09 19:03:36 +0300

    input updated

--- src/input/index.ts
+++ src/input/index.ts
@@ -1,18 +1,15 @@
-import { prefix } from "@/app-constants";
-import { FieldWrapper } from "@/app-shared";
-import { customElement, property } from "lit-element";
-import { html, TemplateResult } from "lit-html";
+import { prefix } from "@constants";
+import { html, customElement, LitElement, property, TemplateResult} from "lit-element";
 
 @customElement(`${prefix}-input`)
-export class Input extends FieldWrapper {
+export class Input extends LitElement {
   @property({ type: Boolean, reflect: true }) multiline = false;
 
   render(): TemplateResult {
-    if (this.multiline) {
-      return this.withFieldWrapper(html`<textarea></textarea>`);
-    }
-    return this.withFieldWrapper(html`
-      <input />
-    `);
+    return html`
+      <field-wrapper color="primary" label="Input">
+        ${ this.multiline ? html`<textarea></textarea>` : html`<input />`}
+      </field-wrapper>
+    `;
   }
 }
