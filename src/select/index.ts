ac3885e351a6a4902d8ce97711e286011ae0fd2e
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-09 19:08:50 +0300

    select component is updated using field wrapper

--- src/select/index.ts
+++ src/select/index.ts
@@ -1,41 +1,18 @@
 import { prefix } from "@/app-constants";
-import { FieldWrapper } from "@/app-shared";
-import { customElement, internalProperty, queryAssignedNodes } from "lit-element";
-import { html, TemplateResult } from "lit-html";
+import { css, CSSResult, customElement, internalProperty, LitElement, html, TemplateResult } from "lit-element";
+import { classMap } from "lit-html/directives/class-map";
 
 @customElement(`${prefix}-select`)
-export class Select extends FieldWrapper {
+export class Select extends LitElement {
   @internalProperty() focused: boolean = false;
 
-  private get _selectStyles(): TemplateResult{
-    return html`
-      <style>
-        .select-container {
-          position: relative;
-          width: 100%;
-        }
-        .select {
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-        }
+  static get styles(): CSSResult{
+    return css`
         .options-container {
           display: none;
         }
-      </style>
-    `;
-  }
 
-  private get _focusedFieldset(): TemplateResult {
-    return html`
-      <style>
-        fieldset {
-          border-bottom-left-radius: 0 !important;
-          border-bottom-right-radius: 0 !important;
-          background-color: var(--c-dark) !important;
-        }
-
-        .options-container {
+        .options-container.open {
           display: block;
           position: absolute;
           width: 100%;
@@ -47,6 +24,7 @@ export class Select extends FieldWrapper {
           margin: 0 -2px;
           background-color: var(--c-dark);
           z-index: 1;
+          box-shadow: 0 16px 32px -1px rgba(0,0,0,0.5);
         }
 
         ::slotted(div) {
@@ -62,38 +40,41 @@ export class Select extends FieldWrapper {
           border-bottom-left-radius: var(--br-md);
           border-bottom-right-radius: var(--br-md);
         }
+    `;
+  }
+
+  private get _focusedFieldset(): TemplateResult {
+    return html`
+      <style>
+        field-wrapper::part(fieldset) {
+          border-bottom-left-radius: 0 !important;
+          border-bottom-right-radius: 0 !important;
+        }
       </style>
     `;
   }
 
-  connectedCallback() {
-    super.connectedCallback();
-    window.addEventListener('click', () => {
-      this.focused = false;
-    });
-  }
-
   render(): TemplateResult {
-    return this.withFieldWrapper(html`
-      ${this._selectStyles}
+    return html`
       ${this.focused ? this._focusedFieldset : ''}
-      <div
-        class="select-container"
-        @click="${this._handleClick}">
-        <div class="select">
-          <input />
-        </div>
-        <div class="options-container">
-          <div class="options">
-            <slot></slot>
+      <field-wrapper color="primary" label="Select">
+        <input @focus="${this._onFocus}" @blur="${this._onBlur}" />
+        <div class="select-container">
+          <div class="options-container ${classMap({open: this.focused})}">
+            <div class="options">
+              <slot></slot>
+            </div>
           </div>
         </div>
-      </div>
-    `);
+      </field-wrapper>
+    `;
   }
 
-  _handleClick(e: any) {
-    e.stopPropagation();
+  _onBlur(e: any) {
+    this.focused = false;
+  }
+
+  _onFocus(e: any) {
     this.focused = true;
   }
 }
