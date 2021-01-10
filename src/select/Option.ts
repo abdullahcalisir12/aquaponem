aea6f5294394f8df917c9164ae2d48a9005b2679
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-10 03:08:32 +0300

    option component created

---
+++ src/select/Option.ts
@@ -0,0 +1,39 @@
+import { prefix } from "@constants";
+import { css, CSSResult, customElement, property, LitElement, html, TemplateResult } from "lit-element";
+
+@customElement(`${prefix}-option`)
+export class Option extends LitElement {
+  @property() value: string|undefined;
+
+  static get styles(): CSSResult{
+    return css`
+      .option {
+        display: block;
+        cursor: pointer;
+        padding: 1rem 1.5rem;
+      }
+
+      .option:hover {
+        background-color: var(--c-bg);
+      }
+    `;
+  }
+
+  render(): TemplateResult {
+    return html`
+      <div class="option" @click=${() => this._click(this.value)}>
+        <slot></slot>
+      </div>
+    `;
+  }
+
+  _click(value: string|undefined) {
+    let event = new CustomEvent('option-clicked', {
+      detail: {
+        message: 'Something important happened',
+        value: value
+      }
+    });
+    this.dispatchEvent(event);
+  }
+}
