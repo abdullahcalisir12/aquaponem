58e579f0b69d585df42958bbd2723af37738179d
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-15 00:33:48 +0300

    input :host missing styles added

--- src/input/index.ts
+++ src/input/index.ts
@@ -1,11 +1,19 @@
 import { prefix } from "@constants";
-import { html, customElement, LitElement, property, TemplateResult} from "lit-element";
+import { html, customElement, LitElement, property, TemplateResult, css} from "lit-element";
 
 @customElement(`${prefix}-input`)
 export class Input extends LitElement {
   @property({ type: Boolean, reflect: true }) multiline = false;
   @property() label!: string;
   @property() color!: string;
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+    `;
+  }
 
   render(): TemplateResult {
     return html`
