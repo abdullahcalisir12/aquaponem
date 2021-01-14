0c829f60e2799f55b80e38521430bfb6fdf8498f
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-15 00:32:42 +0300

    checkbox component updated

--- src/checkbox/index.ts
+++ src/checkbox/index.ts
@@ -1,32 +1,21 @@
-import { css, CSSResultArray, customElement, html, LitElement } from "lit-element";
+import { css, CSSResultArray, customElement, html, LitElement, property } from "lit-element";
 import { prefix } from "@constants";
 import { style } from "./style";
 @customElement(`${prefix}-checkbox`)
 export class Checkbox extends LitElement {
+  @property() name!: string;
+  @property() label!: string;
   static get styles(): CSSResultArray{
     return [
       style,
-      css`
-        .container .checkmark:after {
-          left: 9px;
-          top: 5px;
-          width: 5px;
-          height: 10px;
-          border: solid white;
-          border-width: 0 3px 3px 0;
-          -webkit-transform: rotate(45deg);
-          -ms-transform: rotate(45deg);
-          transform: rotate(45deg);
-        }
-      `
     ];
   }
   render() {
     return html`
       <label class="container">
-        One
-        <input type="checkbox" checked>
+        <input type="checkbox" name=${this.name} checked>
         <span class="checkmark"></span>
+        <slot>${this.label}</slot>
       </label>
     `;
   }
