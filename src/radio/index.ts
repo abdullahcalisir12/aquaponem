1fbb62962e232efab6ac68bbb612a10562d5c492
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-18 16:20:44 +0300

    radio single selection bug fixed

--- src/radio/index.ts
+++ src/radio/index.ts
@@ -1,10 +1,12 @@
 import { css, customElement, html, LitElement, property } from "lit-element";
 import { prefix } from "@constants";
 import { style } from "@/checkbox/style";
+import { ifDefined } from "lit-html/directives/if-defined";
 @customElement(`${prefix}-radio`)
 export class Radio extends LitElement {
   @property() name!: string;
   @property() label!: string;
+  @property({ type: Boolean, reflect: true}) checked: boolean = false;
   static get styles() {
     return [
       style,
@@ -14,10 +16,24 @@ export class Radio extends LitElement {
         }`
     ];
   }
+
+  _change() {
+    if (this.name) {
+      const a = document.getElementsByName(this.name);
+      a.forEach(a => a.removeAttribute('checked'));
+    }
+    this.setAttribute("checked", '');
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    if (this.checked) this._change();
+  }
+
   render() {
     return html`
       <label class="container">
-        <input type="radio" name=${this.name}>
+        <input type="radio" name=${ifDefined(this.name)} .checked=${this.checked} @change=${this._change}>
         <span class="checkmark"></span>
         <slot>${this.label}</slot>
       </label>
